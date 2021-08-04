ecdc683deb1ac625c90cea0a480a69d7201d1adf
Thevindu Hansara <75308046+ThevinduHansara@users.noreply.github.com>
2021-08-04 15:56:38 +0530

    Update alivev2.js

--- plugins/alivev2.js
+++ plugins/alivev2.js
@@ -1,15 +1,14 @@
 const Asena = require('../events');
 const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
-const fs = require('fs');
 const axios = require('axios');
 const Config = require('../config');
 
 if (Config.WORKTYPE == 'public') {
 
-Asena.addCommand({pattern: 'abcd ?(.*)', fromMe: false,dontAddCommandList: true }, (async (message, match) => {
+Asena.addCommand({pattern: 'idka ?(.*)', fromMe: false,dontAddCommandList: true }, (async (message, match) => {
 
-var ttinullimage = await axios.get(`${ Config.ALIVEIMG }$`, { responseType: 'arraybuffer' })
-await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.png, caption: '\n '+Config.ALIVETEXT+' \n'})
+var lasi = await axios.get(`${Config.ALIVEIMG}`, { responseType: 'arraybuffer' })
+await message.sendMessage(Buffer.from(lasi.data), MessageType.image, { mimetype: Mimetype.png, caption: '\n  '+Config.ALIVETEXT+'  \n' })
 
     }));
 }
