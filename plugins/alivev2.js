ff61899f6f1f07d0cee01a2933d0599380e40f77
Thevindu Hansara <75308046+ThevinduHansara@users.noreply.github.com>
2021-08-04 15:49:53 +0530

    Update alivev2.js

--- plugins/alivev2.js
+++ plugins/alivev2.js
@@ -8,7 +8,7 @@ if (Config.WORKTYPE == 'public') {
 
 Asena.addCommand({pattern: 'abcd ?(.*)', fromMe: false,dontAddCommandList: true }, (async (message, match) => {
 
-var ttinullimage = await axios.get(`${+Config.ALIVEIMG+}`, { responseType: 'arraybuffer' })
+var ttinullimage = await axios.get('+Config.ALIVEIMG+', { responseType: 'arraybuffer' })
 await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.png, caption: '\n '+Config.ALIVETEXT+' \n'})
 
     }));
