564e60037cb2ec9f907b37bb1557467932da2247
Thevindu Hansara <75308046+ThevinduHansara@users.noreply.github.com>
2021-08-04 11:21:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -34,6 +34,7 @@ module.exports = {
     BGMFILTER: process.env.BGM_FILTER === undefined ? false : convertToBool(process.env.BGM_FILTER),
     DISBGM: process.env.DISABLE_JID_BGM_FILTER === undefined ? false : process.env.DISABLE_JID_BGM_FILTER,
     AUTOSTICKER: process.env.AUTO_STICKER === undefined ? true : convertToBool(process.env.AUTO_STICKER),
+    CAPTION_KEY: process.env.CP === undefined ?'⚜ 69 ⚜' : process.env.CP
     DISSTICKER: process.env.DISABLE_STICKER === undefined ? false : process.env.DISABLE_STICKER,
     NOLOG: process.env.NO_LOG === undefined ? 'false' : process.env.NO_LOG,
     FULLEVA: process.env.FULL_EVA === undefined ? 'false' : process.env.FULL_EVA,
