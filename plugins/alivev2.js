b4009fed2e456b63357ac4f60776d249481f4315
Thevindu Hansara <75308046+ThevinduHansara@users.noreply.github.com>
2021-08-04 16:22:58 +0530

    Update alivev2.js

--- plugins/alivev2.js
+++ plugins/alivev2.js
@@ -5,7 +5,7 @@ const Config = require('../config');
 
 if (Config.WORKTYPE == 'public') {
 
-Asena.addCommand({pattern: 'fkalive ?(.*)', fromMe: false,dontAddCommandList: true }, (async (message, match) => {
+Asena.addCommand({pattern: 'fkalive', fromMe: false, dontAddCommandList: true }, (async (message, match) => {
 
 var lasi = await axios.get(`${Config.ALIVEIMG}`, { responseType: 'arraybuffer' })
 await message.sendMessage(Buffer.from(lasi.data), MessageType.image, { mimetype: Mimetype.png, caption: Config.ALIVETEXT})
