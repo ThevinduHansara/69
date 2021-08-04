0cfa3d54db3c92be8fc10ea62c46f16bb5a24fcf
Thevindu Hansara <75308046+ThevinduHansara@users.noreply.github.com>
2021-08-04 14:21:13 +0530

    Update alivev2.js

--- plugins/alivev2.js
+++ plugins/alivev2.js
@@ -1,27 +1,14 @@
-/* Copyright (C) 2020 Yusuf Usta.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-WhatsAsena - Yusuf Usta
-Developer & Co-Founder - Phaticusthiccy
-*/
-
 const Asena = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
-const {spawnSync} = require('child_process');
-const Config = require('../config');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
 const fs = require('fs');
-const dil = require('axios');
+const axios = require('axios');
+const Config = require('../config');
 
-const Language = require('../language');
-const Lang = Language.getString('system_stats');
+if (Config.WORKTYPE == 'public') {
 
-Asena.addCommand({pattern: 'aalive', fromMe: false, desc: Lang.ALIVE_DESC}, (async (message, match) => {
-    
-    if (Config.ALIVEMSG == 'default') {
-        
-            await axios.get(`${Config.ALIVEIMG}`, { responseType: 'arraybuffer' }),
-            MessageType.image, 
-            { caption: Config.ALIVETEXT }
-        )
-    }
-}));
+Asena.addCommand({pattern: 'alive2', fromMe: false,dontAddCommandList: true }, (async (message, match) => {
+
+var ttinullimage = await axios.get(`${Config.ALIVEIMG}`, { responseType: 'arraybuffer' })
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.ALIVETEXT})
+    }));
+}
